5584e5bbfbe9b061683cd94eb3992b80a3fb3b72
Povindu Samarasekara <povinduchanmith@gmail.com>
2025-08-10 14:03:35 +0530

    updated program.cs to bind to port 8080

--- Program.cs
+++ Program.cs
@@ -8,6 +8,10 @@ using ProductService.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://*:{port}");
+
+
 // Add services to the container.
 
 builder.Services.AddControllers();
